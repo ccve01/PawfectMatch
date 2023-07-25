013964c0e7af88bc112b20b1affe1333ecc35172
Khalil <khalilfelder04@gmail.com>
2023-07-25 03:55:08 -0400

    setting up multiple animals, just need species variable to work. also added a pink gradient

--- chat.py
+++ chat.py
@@ -1,48 +1,32 @@
 import random
 import json
 
-#from characterai import PyCAI
-# client = PyCAI('c43e694469173dee86270d54d98fab46dee96885')
-#
-# # Dog test: NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g
-#
-# char = 'NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g'
-#
-# # Save tgt and history_external_id
-# # to avoid making a lot of requests
-# chat = client.chat.get_chat(char)
-#
-# history_id = chat['external_id']
-# participants = chat['participants']
-#
-# # In the list of "participants",
-# # a character can be at zero or in the first place
-# if not participants[0]['is_human']:
-#     tgt = participants[0]['user']['username']
-# else:
-#     tgt = participants[1]['user']['username']
-#
-# while True:
-#     message = input('You: ')
-#
-#     data = client.chat.send_message(
-#         char, message, history_external_id=history_id, tgt=tgt
-#     )
-#
-#     name = data['src_char']['participant']['name']
-#     text = data['replies'][0]['text']
-#
-#     print(f"{name}: {text}")
-#     break
-#
+from characterai import PyCAI
 
+#TODO: change the client token so that the name isnt my own
 
-def get_response(msg):
+def get_response(msg, species):
     client = PyCAI('c43e694469173dee86270d54d98fab46dee96885')
 
-    # Dog test: NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g
+    # Dog: NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g
+    # Cat: 9AqE4wu-geCCAddV-NEQGYCWGGlKQLB0h58pmW8dKV0
+    # Bird: LFQaWrz0MKXr_HjKDSUDP4mts3L22AkBGameOWEmOVA
+    # Rabbit: wEKGEN3eLz6mgYFq9kPcwNFuG88KlsQCbR_JaxvvssQ
 
-    char = 'NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g'
+    if species == 'dog':
+        char = 'NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g'
+    elif species == 'cat':
+        char = '9AqE4wu-geCCAddV-NEQGYCWGGlKQLB0h58pmW8dKV0'
+    elif species == 'bird':
+        char = 'LFQaWrz0MKXr_HjKDSUDP4mts3L22AkBGameOWEmOVA'
+    elif species == 'rabbit':
+        char = 'wEKGEN3eLz6mgYFq9kPcwNFuG88KlsQCbR_JaxvvssQ'
+    elif species == None:
+        #defaults to dogbot to test if the type doesnt go through
+        char = 'NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g'
+    else:
+        #defaults to dogbot again
+        char = 'NOk05AzKadfMKlyLs5vFic1FSMtfZs-9oajfdf_An1g'
 
     # Save tgt and history_external_id
     # to avoid making a lot of requests
